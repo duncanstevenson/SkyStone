e602491fe950ee5000ccc798475693c07bc5c07b
duncanstevenson <dmiester1965@gmail.com>
2020-07-19 12:01:14 -0400

    start adding PID module

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/utils/PID.java
@@ -0,0 +1,12 @@
+package org.firstinspires.ftc.teamcode.utils;
+
+public class PID {
+    private double p;
+    private double i;
+    private double d;
+    public PID(double P, Double I, double D){
+        p = P;
+        i = I;
+        d = D;
+    }
+}
